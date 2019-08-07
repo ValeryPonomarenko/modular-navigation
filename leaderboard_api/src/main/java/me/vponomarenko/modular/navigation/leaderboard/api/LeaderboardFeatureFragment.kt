76318ce21090ea722ebb2cafca9ff72419b8fb91
Valery Ponomarenko <valery.v.ponomarenko@yandex.ru>
2019-08-07 16:33:40 -0400

    create an api module for the dynamic feature

--- leaderboard_api/src/main/java/me/vponomarenko/modular/navigation/leaderboard/api/LeaderboardFeatureFragment.kt
+++ leaderboard_api/src/main/java/me/vponomarenko/modular/navigation/leaderboard/api/LeaderboardFeatureFragment.kt
@@ -1,17 +1,10 @@
-package me.vponomarenko.modular.navigation
+package me.vponomarenko.modular.navigation.leaderboard.api
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
-
-/**
- * Author: Valery Ponomarenko
- * Date: 2019-08-07
- * LinkedIn: https://www.linkedin.com/in/ponomarenkovalery
- */
 
 class LeaderboardFeatureFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? =
@@ -20,7 +13,6 @@ class LeaderboardFeatureFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val manager = SplitInstallManagerFactory.create(context)
         if (savedInstanceState == null) {
             childFragmentManager
                 .beginTransaction()
